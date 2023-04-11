3cf5a589f145377a4e489fc2ec48a90d31c9cd8f
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-11 18:57:47 +1000

    Done

--- cases/lc_ann.py
+++ cases/lc_ann.py
@@ -6,10 +6,11 @@ from evaluator import Evaluator
 if __name__ == "__main__":
     configs = []
     for property in ["lc1"]:
-        configs.append({"x":[property], "y":"oc", "machine":"ann", "file_name":"vis_ells"})
+        configs.append({"x":[property], "y":"oc", "machine":"ann"})
 
     ev = Evaluator(
         cofigs=configs,
+        file_name="vis_ells",
         repeat=1,
         folds=10,
         prefix="lc_ann"
