d2ce4c14dd4db64e5d1b3cd43666ca5fcf86ce9a
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-12 00:34:50 +1000

    Done

---
+++ cases/test_intermediate2.py
@@ -0,0 +1,20 @@
+import os
+os.chdir("../")
+from evaluator import Evaluator
+
+
+if __name__ == "__main__":
+    configs = []
+    configs.append({"x":["665", "560", "490"], "y":"oc", "machine":"annx"})
+    configs.append({"x":["665", "560", "490"], "intermediate":["k"], "y":"oc", "machine":"annx"})
+    configs.append({"x":["665", "560", "490"], "intermediate":["p"], "y":"oc", "machine":"annx"})
+
+    ev = Evaluator(
+        cofigs=configs,
+        file_name="vis",
+        repeat=1,
+        folds=10,
+        prefix="tint2"
+    )
+    ev.process()
+    print("Done all")
