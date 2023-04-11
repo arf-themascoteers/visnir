1482d8bc028bb11cdc18de3a2727db18ddc99fd9
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-11 23:54:20 +1000

    Done

--- spectral_dataset.py
+++ spectral_dataset.py
@@ -1,33 +1,27 @@
 import torch
 from torch.utils.data import Dataset
-import numpy as np
 
 
 class SpectralDataset(Dataset):
-    def __init__(self, source=None, x=None, y=None, intermediate=None):
+    def __init__(self, source, x=None, intermediate=None):
+        self.df = source
+        if x is not None:
+            x = list(range(source.shape[1]-1))
+        self.x = source[:,x]
+
         if intermediate is None:
             intermediate = []
-        self.intermediate = intermediate
-        if x is not None and y is not None:
-            y = np.expand_dims(y, axis=1)
-            source = np.concatenate((x,y), axis=1)
-
-        self.df = source
-        self.x = source[:,0:-1]
+        self.intermediate = source[:,intermediate]
         self.y = source[:,-1]
-
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         this_x = torch.tensor(self.x[idx], dtype=torch.float32)
-        soc = torch.tensor(self.y[idx], dtype=torch.float32)
-        items = [this_x, soc]
-        for item_index in self.intermediate:
-            this_item = self.x[item_index]
-            items.append(torch.tensor(this_item, dtype=torch.float32))
-        return items
+        this_intermediate = torch.tensor(self.intermediate[idx], dtype=torch.float32)
+        this_soc = torch.tensor(self.y[idx], dtype=torch.float32)
+        return this_x, this_intermediate, this_soc
 
     def get_y(self):
         return self.y
@@ -38,7 +32,5 @@ class SpectralDataset(Dataset):
     def get_intermediate(self):
         return self.intermediate
 
-    def get_si(self, sif):
-        return sif(self.get_x())
 
 
