1bf0556cb4991c3f1ca530948123cfaceae7d659
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-12 01:33:31 +1000

    Done

---
+++ cases/test_intermediate3.py
@@ -0,0 +1,21 @@
+import os
+os.chdir("../")
+from evaluator import Evaluator
+
+
+if __name__ == "__main__":
+    configs = []
+    configs.append({"x":["665", "560", "490"]+["lc1"], "y":"oc", "min_row":100, "machine":"ann"})
+    configs.append({"x":["665", "560", "490"]+["lc1","lu1"],"min_row":100, "y":"oc", "machine":"annx"})
+    configs.append({"x":["665", "560", "490"]+["lc1"], "intermediate":["lu1"], "min_row":100, "y":"oc", "machine":"annx"})
+    configs.append({"x":["665", "560", "490"], "intermediate":["lc1","lu1"], "min_row":100, "y":"oc", "machine":"annx"})
+
+    ev = Evaluator(
+        cofigs=configs,
+        file_name="vis_ells",
+        repeat=1,
+        folds=10,
+        prefix="tint3"
+    )
+    ev.process()
+    print("Done all")
