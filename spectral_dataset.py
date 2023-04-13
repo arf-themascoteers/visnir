55db51c3c0b372e5972d9076565d205cff185cac
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-14 02:22:18 +1000

    Done

--- spectral_dataset.py
+++ spectral_dataset.py
@@ -4,12 +4,15 @@ from torch.utils.data import Dataset
 
 class SpectralDataset(Dataset):
     def __init__(self, source, x=None, intermediate=None):
+        self.NAN = -9999
         self.df = source
         if x is None:
             x = list(range(source.shape[1]-1))
         self.x = torch.tensor(source[:,x], dtype=torch.float32)
 
         if torch.isnan(self.x).sum() != 0:
+            raise "NAN in X"
+        if self.x[self.x == self.NAN].shape[0] != 0:
             raise "NAN in X"
 
         if intermediate is None:
