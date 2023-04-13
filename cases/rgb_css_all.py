27271320083fb70caee447a3e74f5eb946d9c633
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-14 04:43:33 +1000

    Done

---
+++ cases/rgb_css_all.py
@@ -0,0 +1,34 @@
+import os
+os.chdir("../")
+from evaluator import Evaluator
+
+
+if __name__ == "__main__":
+    rgb = ["665", "560", "490"]
+    configs = []
+
+    configs.append({"x": rgb, "y": "oc"})
+    configs.append({"x": rgb+["clay"], "y": "oc"})
+    configs.append({"x": rgb+["sand"], "y": "oc"})
+    configs.append({"x": rgb+["silt"], "y": "oc"})
+    configs.append({"x": rgb+["clay"+"sand"], "y": "oc"})
+    configs.append({"x": rgb+["sand"+"silt"], "y": "oc"})
+    configs.append({"x": rgb+["silt"+"clay"], "y": "oc"})
+    configs.append({"x": rgb+["clay"+"sand"+"silt"], "y": "oc"})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["clay"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["sand"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["silt"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["clay"+"sand"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["sand"+"silt"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["silt"+"clay"]})
+    configs.append({"x": rgb, "y": "oc", "intermediate":["clay"+"sand"+"silt"]})
+
+    ev = Evaluator(
+        cofigs=configs,
+        repeat=1,
+        folds=10,
+        prefix="rgb_css_all",
+        files = "data/no_empty.csv"
+    )
+    ev.process()
+    print("Done all")
