a0628dbccd8382ddb7d03f5c3622f1afe36baeb0
arif <arif@themascoteers.com>
2023-06-14 23:47:43 +1000

    done

--- play.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn.functional as F
-
-x1 = torch.tensor([3,3,3,3,3,3], dtype=torch.float32)
-x2 = torch.tensor([1,1,1,1,1,1], dtype=torch.float32)
-
-z = F.mse_loss(x1,x2)
-
-print(z)
