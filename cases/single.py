65f6615379b453ae9986ee1812287203aa624191
arif <arif@themascoteers.com>
2023-06-21 02:24:11 +1000

    done

--- cases/single.py
+++ cases/single.py
@@ -4,13 +4,24 @@ from evaluator import Evaluator
 
 
 if __name__ == "__main__":
-    alpha = 0.1
     rgb = ["665", "560", "490"]
     configs = []
+
+    ev = Evaluator(
+        cofigs=[{"x":rgb, "y":"oc"}],
+        repeat=1,
+        folds=10,
+        prefix=f"oc",
+        files="data/vis_with_empty.csv"
+    )
+    ev.process()
+
+
 
     for property in ["phc","phh","ec","caco3","p","n","k","stones"]:
         configs.append({"x":rgb, "y":"oc",  "intermediate":[property]})
 
+    alpha = 0.1
     while alpha <= 0.7:
         ev = Evaluator(
             cofigs=configs,
