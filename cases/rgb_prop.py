7bbecfb4462d369f49236fefe00491a82699ac2d
arif <arif@themascoteers.com>
2023-06-21 12:04:22 +1000

    done

---
+++ cases/rgb_prop.py
@@ -0,0 +1,23 @@
+import os
+os.chdir("../")
+from evaluator import Evaluator
+
+
+if __name__ == "__main__":
+    rgb = ["665", "560", "490"]
+    configs = []
+
+    for property in ["phc","phh","ec","caco3","p","n","k","stones"]:
+        configs.append({"x":rgb, "y":property})
+
+    ev = Evaluator(
+        cofigs=configs,
+        repeat=1,
+        folds=10,
+        prefix=f"rgb_prop",
+        files="data/vis_with_empty.csv",
+        alpha=0
+    )
+    ev.process()
+
+    print("Done all")
